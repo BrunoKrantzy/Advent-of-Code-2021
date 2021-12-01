0caf34af8ba87cc17124fcda4838455b0bf4ac39
BrunoKrantzy <bruno.denis@gmail.com>
2021-12-01 15:32:28 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,16 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    val testInput = readInput("input")
+
+    var base = testInput[0].toInt()
+    var c = 0
+    for (i in 1 .. testInput.size - 1) {
+        val suite = testInput[i].toInt()
+        if (suite > base) c++
+        base = suite
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(c)
 }
+
+
